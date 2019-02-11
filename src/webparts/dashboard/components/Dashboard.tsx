72daa7d9572cdb68442e0f100d9a1148dec61434
maddy1690 <mahendra.gohel@outlook.com>
2019-02-11 18:45:34 +0530

    initial commit

---
+++ src/webparts/dashboard/components/Dashboard.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import styles from './Dashboard.module.scss';
+import { IDashboardProps } from './IDashboardProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+export default class Dashboard extends React.Component<IDashboardProps, {}> {
+  public render(): React.ReactElement<IDashboardProps> {
+    return (
+      <div >
+        
+      </div>
+    );
+  }
+}
